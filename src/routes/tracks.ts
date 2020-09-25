828fb1be4cfb0b859736aabaab026d27def3ff64
kartik <kartikbhalla12@gmail.com>
2020-09-25 16:33:58 +0530

    tracks: split tracks to hls segments and serve from aws s3 directly
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/routes/tracks.ts
+++ src/routes/tracks.ts
@@ -1,20 +1,19 @@
 import { Router, Request } from 'express';
 import Joi from 'joi';
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import multer from 'multer';
 import aws from 'aws-sdk';
-// import multerS3 from 'multer-s3';
 import config from 'config';
-// import express from 'express';
-// import fs from 'fs';
 
 const ID: string = config.get('awsAccessKeyId');
 const SECRET: string = config.get('awsSecretAccessKey');
 const BUCKET_NAME: string = config.get('awsBucketName');
+const REGION: string = config.get('awsRegion');
 
 const s3 = new aws.S3({
 	accessKeyId: ID,
 	secretAccessKey: SECRET,
+	region: REGION,
 });
 
 const router = Router();
@@ -55,73 +54,58 @@ const newTrack = async (track: Track) => {
 	await newTrack.save();
 };
 
-router.get('/', async (req, res) => {
-	// const head = {
-	// 	'Content-Type': 'audio/mp4',
-	// 	// 'Content-Length': 500,
-	// };
-	// res.writeHead(200, head);
-	const audioStream = s3
-		.getObject({
-			Bucket: BUCKET_NAME,
-			Key: '1600873298014',
-		})
-		.createReadStream();
+router.get(
+	'/',
+	async (
+		req: Request<{}, {}, {}, { images: 'none' | 'single' | 'all' }>,
+		res
+	) => {
+		const imageQuery = req.query.images;
+		const tracks = await Track.find();
 
-	// stream.pipe(res);
+		if (tracks) {
+			let response: Track[] = [];
 
-	// audioStream.on('error', function (err) {
-	// 	// NoSuchKey: The specified key does not exist
-	// 	console.error(err);
-	// });
+			if (imageQuery && imageQuery !== 'none') {
+				if (imageQuery === 'single') {
+					for (let track of tracks) {
+						const res = track.toJSON();
+						const image = res.images[0];
 
-	audioStream.pipe(res);
+						const imageBuffer = await s3
+							.getObject({
+								Bucket: BUCKET_NAME,
+								Key: image.imageKey.toString(),
+							})
+							.promise();
 
-	// res.send('dasdas');
+						image.src = imageBuffer.Body!.toString('base64');
+						response.push(res);
+					}
+				}
+				if (imageQuery === 'all') {
+					for (let track of tracks) {
+						const res = track.toJSON();
 
-	// const imgs: any = [];
-	// const getImages = async () => {
-	// 	for (let image of list[0].images as Array<{ imageKey: string }>) {
-	// 		const base64 = await s3
-	// 			.getObject({
-	// 				Bucket: BUCKET_NAME,
-	// 				Key: image.imageKey,
-	// 			})
-	// 			.promise();
-	// 		imgs.push(base64.Body!.toString('base64'));
-	// 	}
+						for (let image of res.images) {
+							const imageBuffer = await s3
+								.getObject({
+									Bucket: BUCKET_NAME,
+									Key: image.imageKey.toString(),
+								})
+								.promise();
 
-	// 	// (list[0].images as Array<{ imageKey: string }>).forEach(async image => {
-	// 	// 	const base64 = await s3
-	// 	// 		.getObject({
-	// 	// 			Bucket: BUCKET_NAME,
-	// 	// 			Key: image.imageKey,
-	// 	// 		})
-	// 	// 		.promise();
-	// 	// 	imgs.push(base64.Body!.toString('base64'));
-	// 	// 	// console.log(base64);
-	// 	// });
-	// };
+							image.src = imageBuffer.Body!.toString('base64');
+						}
+						response.push(res);
+					}
+				}
+			}
 
-	// await // let image = await s3
-	// 	.getObject({
-	// 		Bucket: BUCKET_NAME,
-	// 		Key: list[0].images[0].imageKey,
-	// 	})
-	// 	.promise();
-
-	// objectStream.on('data')
-	// console.log(objectPromise.Body);
-	// const images = await Promise.all(imgs);
-	// console.log(images);
-	// console.log(imgs);
-	// await getImages();
-	// res.send({ ...list[0], imagesSrc: imgs }).status(200);
-
-	// objectPromise.on('httpDownloadProgress', (progress, data) =>
-	// 	console.log(progress, data)
-	// );
-});
+			res.send(response);
+		}
+	}
+);
 
 router.get(
 	'/:id',
@@ -170,9 +154,27 @@ router.get(
 	}
 );
 
+router.get('/:id/play', async (req: Request<{ id: string }>, res) => {
+	const trackId = req.params.id;
+
+	const data = await Track.findOne({
+		_id: trackId,
+	});
+
+	const track: Track = data!.toJSON();
+
+	const url = await s3.getSignedUrlPromise('getObject', {
+		Bucket: BUCKET_NAME,
+		Key: track.trackFile.trackKey,
+		Expires: 360,
+	});
+
+	res.send(url);
+});
+
 router.post(
 	'/',
-	parseFormData.fields([{ name: 'track', maxCount: 1 }, { name: 'images' }]),
+	parseFormData.fields([{ name: 'track' }, { name: 'images' }]),
 	async (
 		req: Request<{}, {}, { name: string; duration: number; artists: string[] }>,
 		res
@@ -214,28 +216,39 @@ router.post(
 				});
 			}
 
-			// upload track
-			const trackKey = `${name}/${track[0].originalname}`;
-			await s3
-				.putObject({
-					Bucket: BUCKET_NAME,
-					Key: trackKey,
-					Body: track[0].buffer,
-				})
-				.promise();
+			for (let trackFile of track) {
+				const trackKey = `${name}/tracks/${trackFile.originalname}`;
+				if (trackFile.mimetype.includes('mp2t')) {
+					await s3
+						.putObject({
+							Bucket: BUCKET_NAME,
+							Key: trackKey,
+							Body: trackFile.buffer,
+							ACL: 'public-read',
+						})
+						.promise();
+				} else if (trackFile.mimetype.includes('mpegurl')) {
+					await s3
+						.putObject({
+							Bucket: BUCKET_NAME,
+							Key: trackKey,
+							Body: trackFile.buffer,
+						})
+						.promise();
 
-			trackInDB = {
-				name: track[0].originalname,
-				size: track[0].size,
-				trackKey,
-			};
-
+					trackInDB = {
+						name: trackFile.originalname,
+						size: trackFile.size,
+						trackKey,
+					};
+				}
+			}
 			await newTrack({
 				artists,
 				duration,
 				name,
 				images: imagesInDB,
-				trackFile: trackInDB,
+				trackFile: trackInDB!,
 			});
 			return res.status(200).send('Done');
 		}
